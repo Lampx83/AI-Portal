87ea3dfafbc504546e04078147cb3b90e4dfa4dd
Lampx83 <lampx@neu.edu.vn>
2025-07-06 11:31:35 +1000

    test CI CD

--- components/expert-view.tsx
+++ components/expert-view.tsx
@@ -39,7 +39,7 @@ const expertsData = [
   },
   {
     name: "GS.TS. Hoàng Văn Cường",
-    title: "Bất động sản",
+    title: "Chuyên gia Bất động sản",
     department: "Khoa Bất động sản và Kinh tế tài nguyên",
     tags: ["Thị trường BĐS", "Quy hoạch đô thị", "Đầu tư"],
   },
