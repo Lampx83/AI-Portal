b0e246643ec2f92284e2b4b3131649c88ef9337d
Lampx83 <lampx@neu.edu.vn>
2025-09-05 09:19:55 +0700

    Fix

--- lib/research-assistants.ts
+++ lib/research-assistants.ts
@@ -68,7 +68,7 @@ export const researchAssistants: ResearchAssistant[] = [
         Icon: FileText,
         bgColor: "bg-cyan-100 dark:bg-cyan-900/30",
         iconColor: "text-cyan-600 dark:text-cyan-400",
-        baseUrl: "https://research.neu.edu.vn/api/demo_agent/v1"
+        baseUrl: "http:/localhost:3000/api/demo_agent/v1"
     },
     {
         "name": "Chuyên gia",
